1ccf80ee6c3c431f6c3bd90b451ff564d48d55b2
Steve Munoz <stevomunoz@yahoo.com>
2021-03-18 18:03:24 -0500

    added a heart icon and cart icon when hovering over image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Image({className, img}){
+    const [hovered, setHovered] = useState(false)
+    {/* ↓ what is happenening below with && is that if hovered is true 
+    then render <i> if its false then it wont render anything*/}
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
     return(
-        <div className = {`${className} image-container`}>
+        <div className = {`${className} image-container`}
+        onMouseEnter={()=> setHovered(true)}
+        onMouseLeave={()=> setHovered(false)}>
             <img src={img.url} className = "image-grid"/>
+            {heartIcon}
+            {cartIcon}
 
         </div>
     )
